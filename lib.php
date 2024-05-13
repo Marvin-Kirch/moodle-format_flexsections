c5a3eae8f9d9013f20dce24d19c8078d6fdd6ed1
Marina Glancy <marina@moodle.com>
2024-05-13 09:38:03 +0100

    Start supporting 4.4

--- lib.php
+++ lib.php
@@ -151,12 +151,37 @@ class format_flexsections extends core_courseformat\base {
     }
 
     /**
+     * Set if the current format instance will show multiple sections or an individual one.
+     *
+     * Some formats has the hability to swith from one section to multiple sections per page,
+     * output components will use this method to know if the current display is a single or
+     * multiple sections.
+     *
+     * @return int zero for all sections or the sectin number
+     */
+    public function get_section_number(): int {
+
+        if ($this->singlesection === null) {
+            // Convert null to zero, to guarantee all the sections are displayed.
+            return 0;
+        }
+
+        return $this->singlesection;
+    }
+
+    /**
      * Generate the title for this section page.
      *
      * @return string the page title
      */
     public function page_title(): string {
-        return get_string('topicoutline');
+        global $CFG;
+        if ((int)$CFG->branch >= 404) {
+            // TODO it is possible it is not used anymore. Review.
+            return 'Topic outline';
+        } else {
+            return get_string('topicoutline');
+        }
     }
 
     /**
@@ -1429,6 +1454,10 @@ function format_flexsections_add_back_link_to_cm(): ?cm_info {
  * @return string
  */
 function format_flexsections_before_footer() {
+    // This is an implementation of a legacy callback that will only be called in older Moodle versions.
+    // It will not be called in Moodle versions that contain the hook core\hook\output\before_footer_html_generation,
+    // instead, the callback format_flexsections\local\hooks\output\before_footer_html_generation::callback will be executed.
+
     global $OUTPUT;
     if ($cm = format_flexsections_add_back_link_to_cm()) {
         return $OUTPUT->render_from_template('format_flexsections/back_link_in_cms', [
